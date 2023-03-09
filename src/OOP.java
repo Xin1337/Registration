e7709b57a27df5e8185611dbe13e9a2dec41b02e
Nix <drlftg0d321@gmail.com>
2023-03-09 18:12:56 +0800

    Initial commit

---
+++ src/OOP.java
@@ -0,0 +1,30 @@
+public class OOP {
+
+    private int age;
+    private String names;
+    private String course;
+    private String year;
+
+    public OOP(int age, String names, String course, String year) {
+        this.age = age;
+        this.names = names;
+        this.course = course;
+        this.year = year;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getNames() {
+        return names;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public String getYear() {
+        return year;
+    }
+}
---
+++ src/Registration.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+import java.util.LinkedList;
+
+/**
+ * @author Dominic
+ * @version 1.0
+ * @since 2023-03-09
+ * @see OOP
+ * @see LinkedList
+ * @see Scanner
+ * @see System
+ * Total Time: 2 hours
+ * Description: This program is a registration system that allows the user to add, view and remove students from the system.
+ */
+
+public class Registration extends OOP {
+    public Registration(int age, String names, String course, String year) {
+        super(age, names, course, year);
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter("\n");
+        LinkedList<Registration> students = new LinkedList<>();
+
+        while (true) {
+            System.out.println("Welcome to the registration system!");
+            System.out.println("1. Add student");
+            System.out.println("2. View Student");
+            System.out.println("3. Remove Student");
+            System.out.println("4. Exit");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter the student name: ");
+                    String name = scanner.next();
+                    System.out.println("Enter the student age: ");
+                    int age = scanner.nextInt();
+                    System.out.println("Enter the student course: ");
+                    String course = scanner.next();
+                    System.out.println("Enter the student year: ");
+                    String year = scanner.next();
+                    if (students.contains(name)) {
+                        System.out.println("Student already exists!");
+                        break;
+                    } else {
+                        students.add(students.size(), new Registration(age, name, course, year));
+                        System.out.println("Student added!");
+                    }
+                    break;
+                case 2:
+                    if (students.size() == 0) {
+                        System.out.println("No students in the system!");
+                    } else {
+                        for (Registration student : students) {
+                            System.out.printf("Name: %s, Age: %s, Course: %s, Year: %s\n", student.getNames(), student.getAge(), student.getCourse(), student.getYear());
+                        }
+                    }
+                    break;
+                case 3:
+                    System.out.println("Enter the student name: ");
+                    String found = scanner.next();
+                    for (Registration student : students) {
+                        if (student.getNames().equals(found)) {
+                            students.remove(student);
+                            System.out.printf("%s has been removed from the system!\n", found);
+                            break;
+                        } else {
+                            System.out.println("Student not found!");
+                            break;
+                        }
+                    }
+                    break;
+                case 4:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice!");
+                    break;
+            }
+        }
+    }
+}
