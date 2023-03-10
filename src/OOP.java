0960744ec3e095d7143b2953a03284fec2c438cd
Nix <drlftg0d321@gmail.com>
2023-03-10 08:50:49 +0800

    Address gender and contact number

--- src/OOP.java
+++ src/OOP.java
@@ -1,23 +1,43 @@
 public class OOP {
-
+    // Variables
     private int age;
     private String names;
+    private String gender;
+    private int contactNumber;
+    private String address;
     private String course;
     private String year;
 
-    public OOP(int age, String names, String course, String year) {
+    // Constructor for the OOP class
+    public OOP(int age, String names, String gender, int contactNumber, String address, String course, String year) {
         this.age = age;
         this.names = names;
+        this.gender = gender;
+        this.contactNumber = contactNumber;
+        this.address = address;
         this.course = course;
         this.year = year;
     }
 
+    // Getters
     public int getAge() {
         return age;
     }
 
-    public String getNames() {
+    public String getName() {
         return names;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getContactNumber() {
+        return contactNumber;
+    }
+
+    public String getAddress() {
+        return address;
     }
 
     public String getCourse() {
--- src/Registration.java
+++ src/Registration.java
@@ -9,13 +9,16 @@ import java.util.LinkedList;
  * @see LinkedList
  * @see Scanner
  * @see System
- * Total Time: 3 hours
+ * Total hours spent on this project: 3 hours
+ * Ayoko na mag IT
+ * Patya nalang ko
  * Description: This program is a registration system that allows the user to add, view and remove students from the system.
  */
 
 public class Registration extends OOP {
-    public Registration(int age, String names, String course, String year) {
-        super(age, names, course, year);
+    // This constructor inherits the variables from the OOP class
+    public Registration(int age, String names, String gender, int contactNumber, String address, String course, String year) {
+        super(age, names, gender, contactNumber, address, course, year);
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -39,28 +42,33 @@ public class Registration extends OOP {
                     String name = scanner.next();
                     System.out.println("Enter the student age: ");
                     int age = scanner.nextInt();
+                    System.out.println("Enter Gender: ");
+                    String gender = scanner.next();
+                    System.out.println("Enter the student contact number: ");
+                    int contactNumber = scanner.nextInt();
+                    System.out.println("Enter the student address: ");
+                    String address = scanner.next();
                     System.out.println("Enter the student course: ");
                     String course = scanner.next();
                     System.out.println("Enter the student year: ");
                     String year = scanner.next();
                     // Check if the student already exists
-                    if (students.parallelStream().anyMatch(student -> student.getNames().equalsIgnoreCase(name))) { // Thank you Denom for this line of code!
+                    if (students.parallelStream().anyMatch(student -> student.getName().equalsIgnoreCase(name))) { // Thank you Denom for this line of code!
                         System.out.println("Student already exists!");
-                        break;
                     } else {
                         // Add the student to the LinkedList
-                        students.add(new Registration(age, name, course, year));
+                        students.add(new Registration(age, name, gender, contactNumber, address, course, year));
                         System.out.printf("%s has been added to the system!\n", name);
-                        break;
                     }
+                    break;
                 case 2:
                     // Check if the LinkedList is empty
                     if (students.size() == 0) {
                         System.out.println("No students in the system!");
                     } else {
                         // Print out the students
-                        for (Registration student : students) {
-                            System.out.printf("Name: %s, Age: %s, Course: %s, Year: %s\n", student.getNames(), student.getAge(), student.getCourse(), student.getYear());
+                        for (Registration student : students) { //Goes through the LinkedList and prints out the students
+                            System.out.printf("Name: %s, Age: %s, Gender: %s, Contact Number: %s, Address: %s, Course: %s, Year: %s\n", student.getName(), student.getAge(), student.getGender(), student.getContactNumber(), student.getAddress(), student.getCourse(), student.getYear());
                         }
                     }
                     break;
@@ -68,8 +76,8 @@ public class Registration extends OOP {
                     System.out.println("Enter the student name: ");
                     String found = scanner.next();
                     // Check if the student exists
-                    if (students.parallelStream().anyMatch(student -> student.getNames().equalsIgnoreCase(found))) { // Thank you Denom for this line of code!
-                        students.removeIf(student -> student.getNames().equalsIgnoreCase(found)); //Removes the student from the LinkedList
+                    if (students.parallelStream().anyMatch(student -> student.getName().equalsIgnoreCase(found))) { // Thank you Denom for this line of code!
+                        students.removeIf(student -> student.getName().equalsIgnoreCase(found)); //Removes the student from the LinkedList
                         System.out.printf("%s has been removed from the system!\n", found);
                     } else {
                         System.out.println("Student not found!");
